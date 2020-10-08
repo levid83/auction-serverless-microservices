2e18c500972d206c62778c376ed59eb9cc423ec3
Levente Danel <danellevente@yahoo.com>
2020-10-08 22:23:20 +0300

    extract all db operations into AuctionRepository

--- services/auction/src/lib/repositories/AuctionRepository.js
+++ services/auction/src/lib/repositories/AuctionRepository.js
@@ -6,6 +6,51 @@ export default class AuctionRepository {
     this._tableName = tableName;
   }
 
+  async getAuctionById(id) {
+    const result = await this._db
+      .get({
+        TableName: this._tableName,
+        Key: { id },
+      })
+      .promise();
+    return result.Item;
+  }
+
+  async getAuctionsByStatus(status) {
+    const params = {
+      TableName: this._tableName,
+      IndexName: "statusAndEndDate",
+      KeyConditionExpression: "#status = :status",
+      ExpressionAttributeValues: {
+        ":status": status,
+      },
+      ExpressionAttributeNames: {
+        "#status": "status",
+      },
+    };
+
+    const result = await this._db.query(params).promise();
+    return result.Items;
+  }
+
+  async getEndedAuctions() {
+    const params = {
+      TableName: this._tableName,
+      IndexName: "statusAndEndDate",
+      KeyConditionExpression: "#status = :status AND endingAt <= :now",
+      ExpressionAttributeValues: {
+        ":status": "OPEN",
+        ":now": new Date().toISOString(),
+      },
+      ExpressionAttributeNames: {
+        "#status": "status",
+      },
+    };
+
+    const result = await this._db.query(params).promise();
+    return result.Items;
+  }
+
   async saveAuction({ email, title }) {
     const now = new Date();
     const endDate = new Date();
@@ -31,5 +76,52 @@ export default class AuctionRepository {
       .promise();
 
     return auction;
+  }
+
+  async setAuctionPictureUrl(id, pictureUrl) {
+    const params = {
+      TableName: this._tableName,
+      Key: { id },
+      UpdateExpression: "set pictureUrl = :pictureUrl",
+      ExpressionAttributeValues: {
+        ":pictureUrl": pictureUrl,
+      },
+      ReturnValues: "ALL_NEW",
+    };
+
+    const result = await this._db.update(params).promise();
+    return result.Attributes;
+  }
+
+  async updateHighestBid({ id, email, amount }) {
+    const params = {
+      TableName: this._tableName,
+      Key: { id },
+      UpdateExpression:
+        "set highestBid.amount = :amount, highestBid.bidder = :bidder",
+      ExpressionAttributeValues: {
+        ":amount": amount,
+        ":bidder": email,
+      },
+      ReturnValues: "ALL_NEW",
+    };
+    const result = await this._db.update(params).promise();
+
+    return result.Attributes;
+  }
+
+  async closeAuction(id) {
+    const params = {
+      TableName: this._tableName,
+      Key: { id: id },
+      UpdateExpression: "set #status = :status",
+      ExpressionAttributeValues: {
+        ":status": "CLOSED",
+      },
+      ExpressionAttributeNames: {
+        "#status": "status",
+      },
+    };
+    await this._db.update(params).promise();
   }
 }
